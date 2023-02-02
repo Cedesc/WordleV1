5327c6dd6da563cfc657717d6a0bde1489c865b3
Christian <irgendwas@nix.de>
2023-02-02 15:56:42 +0100

    documentation extended

--- src/main/kotlin/com/example/demo/model/LetterBox.kt
+++ src/main/kotlin/com/example/demo/model/LetterBox.kt
@@ -2,11 +2,14 @@ package com.example.demo.model
 
 /**
  * Representation of a letter in the [WordBoard].
+ *
+ * @property letter The shown letter in the box.
+ * @property state The color of the box.
  */
 class LetterBox {
 
     /**
-     * Shown letter. Initialized with null.
+     * Shown letter. Initialized with a space character.
      */
     var letter: Char = ' '
         set(value) {
@@ -15,8 +18,9 @@ class LetterBox {
         }
 
     /**
-     * Color of the box.
+     * Color of the box. Initialized with 0 (black).
      *
+     * The value can be one of the following:
      * - 0: black (not checked yet)
      * - 1: grey (wrong letter)
      * - 2: yellow (correct letter, wrong place)
@@ -30,6 +34,12 @@ class LetterBox {
 
 
     /**
+     * Check if the given result word contains the [letter].
+     *
+     * If the word contains the letter, the [state] is set to 2 (yellow).
+     * If the word does not contain the letter, the [state] is set to 1 (grey).
+     *
+     * @param resultWord the word to check for the letter.
      * @return True if resultWord contains [LetterBox.letter], False otherwise.
      */
     fun checkIfLetterIsInWord(resultWord: String): Boolean {
@@ -43,6 +53,11 @@ class LetterBox {
     }
 
     /**
+     * Compare the given input letter to the [letter].
+     *
+     * If the input letter is the same as the letter, the [state] is set to 3 (green).
+     *
+     * @param inputLetter the letter to compare to the [letter].
      * @return True if the given letter is the same as [LetterBox.letter], False otherwise.
      */
     fun compareLetter(inputLetter: Char): Boolean {
@@ -55,6 +70,10 @@ class LetterBox {
         return equal
     }
 
+    /**
+     * @return A string representation of the [LetterBox] in the format "LetterBox(letter=X, state=Y)",
+     * where X is the letter and Y is the state.
+     */
     override fun toString(): String {
         return "LetterBox(letter=$letter, state=$state)"
     }
