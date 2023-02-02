4d520d6bf44d1b2cbb344b0e123c664a424979d3
Christian <irgendwas@nix.de>
2023-02-02 10:01:32 +0100

    wrote compareLetter() and checkIfLetterIsInWord wrote setter for letter and state added toString()

--- src/main/kotlin/com/example/demo/model/LetterBox.kt
+++ src/main/kotlin/com/example/demo/model/LetterBox.kt
@@ -4,12 +4,15 @@ package com.example.demo.model
  * Representation of a letter in the [WordBoard].
  */
 class LetterBox {
-    // TODO: write tests for constructor
 
     /**
      * Shown letter. Initialized with null.
      */
-    private var letter: Char? = null
+    var letter: Char = ' '
+        set(value) {
+            if ("abcdefghijklmnopqrstuvwxyz ".contains(value))
+                field = value
+        }
 
     /**
      * Color of the box.
@@ -19,23 +22,40 @@ class LetterBox {
      * - 2: yellow (correct letter, wrong place)
      * - 3: green (correct letter, correct place)
      */
-    private var state: Int = 0
+    var state: Int = 0
+        set(value) {
+            field = maxOf(0, minOf(value, 3))
+        }
 
 
     /**
      * @return True if resultWord contains [LetterBox.letter], False otherwise.
      */
-    private fun checkIfLetterIsInWord(resultWord: String): Boolean {
-        // TODO: Not yet implemented.
-        return false
+    fun checkIfLetterIsInWord(resultWord: String): Boolean {
+        // check
+        val contains = resultWord.contains(letter)
+
+        // update state
+        state = if (contains) 2 else 1
+
+        return contains
     }
 
     /**
      * @return True if the given letter is the same as [LetterBox.letter], False otherwise.
      */
-    private fun compareLetter(letter: Char): Boolean {
-        // TODO: Not yet implemented.
-        return false
+    fun compareLetter(inputLetter: Char): Boolean {
+        // compare
+        val equal = inputLetter == letter
+
+        // update state
+        if (equal) state = 3
+
+        return equal
+    }
+
+    override fun toString(): String {
+        return "LetterBox(letter=$letter, state=$state)"
     }
 
 }
