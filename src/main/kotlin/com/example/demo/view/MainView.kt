cb22d50cccdca8b19706c81478539b8058a26ff3
Christian <irgendwas@nix.de>
2023-02-04 18:19:30 +0100

    added full beta view for the keyBoard

--- src/main/kotlin/com/example/demo/view/MainView.kt
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -1,12 +1,71 @@
 package com.example.demo.view
 
 import com.example.demo.style.Styles
+import javafx.geometry.Insets
+import javafx.geometry.Orientation
 import tornadofx.*
 
-class MainView : View("Hello TornadoFX") {
-    override val root = hbox {
-        label(title) {
+class MainView : View("WordleX3Z") {
+    // TODO: add Tests? Is it possible or meaningful to have tests for this?
+
+    override val root = vbox {
+
+        label("Hello World") {
             addClass(Styles.heading)
         }
+
+        // horizontal line
+        separator(Orientation.HORIZONTAL) {
+            padding = Insets(5.0, 10.0, 25.0, 10.0)
+        }
+
+
+        // TODO: arrange rows in a way that the keyboard has a symmetric shape
+        // create keyboard
+        vbox {
+
+            // first row of the keyboard
+            hbox {
+                // create first row of letter keys
+                for (letter in "QWERTZUIOP") { button(letter.toString()) {
+                    addClass(Styles.keyboardButton)
+                    action { println(letter) } }
+                }
+            }
+
+            // second row of the keyboard
+            hbox {
+                // create second row of letter keys
+                for (letter in "ASDFGHJKL") { button(letter.toString()) {
+                    addClass(Styles.keyboardButton)
+                    action { println(letter) } }
+                }
+            }
+
+            // third row of the keyboard
+            hbox {
+                // create enter button
+                button("ENTER") {
+                    addClass(Styles.keyboardButton)
+                    action { println("Enter") }
+                }
+
+                // create third row of letter keys
+                for (letter in "YXCVBNM") { button(letter.toString()) {
+                    addClass(Styles.keyboardButton)
+                    action { println(letter) } }
+                }
+
+                // create back button
+                button("BACK") {
+                    action { println("Back") }
+                    addClass(Styles.keyboardButton)
+                }
+            }
+
+
+        }
+
     }
+
 }
