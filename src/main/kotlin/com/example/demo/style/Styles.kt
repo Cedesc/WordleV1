db53a1ad6c2528fbc0bf942c0331ae81265cb7ac
Christian <irgendwas@nix.de>
2023-02-04 19:27:51 +0100

    added wordBoard and keyBoard removed heading

--- src/main/kotlin/com/example/demo/style/Styles.kt
+++ src/main/kotlin/com/example/demo/style/Styles.kt
@@ -8,24 +8,28 @@ import tornadofx.px
 
 class Styles : Stylesheet() {
     companion object {
-        val heading by cssclass()
+        val wordBoard by cssclass()
+        val keyBoard by cssclass()
         val keyboardButton by cssclass()
     }
 
     init {
-        label and heading {
-            padding = box(10.px)
-            fontSize = 20.px
-            fontWeight = FontWeight.BOLD
-        }
-    }
 
-    init {
+        wordBoard {
+            padding = box(40.px)
+        }
+
+        keyBoard {
+            padding = box(40.px)
+        }
+
         button and keyboardButton {
             padding = box(10.px)
             fontSize = 20.px
+            fontWeight = FontWeight.SEMI_BOLD
             minWidth = 40.px
             minHeight = 40.px
         }
+
     }
 }
