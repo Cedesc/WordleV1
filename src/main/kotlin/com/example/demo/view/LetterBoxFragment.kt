14b7c783bcb699bf8e37072fe28c395b0c303205
Christian <irgendwas@nix.de>
2023-02-06 22:17:06 +0100

    extended documentation

--- src/main/kotlin/com/example/demo/view/LetterBoxFragment.kt
+++ src/main/kotlin/com/example/demo/view/LetterBoxFragment.kt
@@ -9,18 +9,42 @@ import javafx.scene.text.Font
 import javafx.scene.text.FontWeight
 import tornadofx.*
 
+/**
+ * LetterBoxFragment is a [Fragment] that displays a [LetterBox].
+ */
 class LetterBoxFragment : Fragment() {
+    /**
+     * letterBox is the [LetterBox] that will be displayed by this fragment.
+     */
     val letterBox: LetterBox by param()
 
+    /**
+     * letterProperty is a [SimpleStringProperty] that holds the current letter of the letter box.
+     * It is bound to the letterProperty of the letterBox.
+     */
     private val letterProperty = SimpleStringProperty(letterBox.letter.toString())
+    /**
+     * stateProperty is a [SimpleIntegerProperty] that holds the current state of the letter box.
+     * It is bound to the stateProperty of the letterBox.
+     */
     private val stateProperty = SimpleIntegerProperty(letterBox.state)
 
+    /**
+     * Initialize the letterProperty and stateProperty to be bound to the corresponding properties of the letterBox.
+     */
     init {
         letterProperty.bind(letterBox.letterProperty)
         stateProperty.bind(letterBox.stateProperty)
     }
 
 
+    /**
+     * The root of the fragment.
+     * A StackPane that contains a rectangle and a label.
+     * The rectangle represents the background of the letter box and its fill color changes based on the state of the
+     * letter box.
+     * The label displays the letter of the letter box.
+     */
     override val root = stackpane {
         rectangle {
             width = 50.0
@@ -29,6 +53,10 @@ class LetterBoxFragment : Fragment() {
             stroke = Color.LIGHTGRAY
             strokeWidth = 1.0
 
+            /**
+             * Listener for changes in the stateProperty.
+             * When the state of the letter box changes, the fill color of the rectangle changes accordingly.
+             */
             stateProperty.addListener { _, _, newValue ->
                 when (newValue.toInt()) {
                     0 -> fill = Color.BLACK
@@ -39,6 +67,9 @@ class LetterBoxFragment : Fragment() {
             }
         }
 
+        /**
+         * Label that displays the letter of the letter box.
+         */
         label(letterProperty) {
             textFill = Color.WHITE
             font = Font.font("Arial", FontWeight.BOLD, 14.0)
